03081375751d393495008175e37fe26472f1b825
Junior <jorge-luiz.junior@siemens.com>
2026-02-01 19:09:32 -0300

    Ajuste de verificação das variáveis

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -2,27 +2,36 @@ function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-    let numero;
-    let sorteados = [];
 
-    for (let i=1; i<=quantidade;i++){
-        numero = gerarNumeroAleatorio(de,ate);
-        while(sorteados.includes(numero)){
+    if(quantidade > (ate-de+1)){
+        alert('A quantidade de números não pode ser maior que o intervalo');
+        return
+    }
+    if (de >= ate){
+        alert('O limite inferior não pode ser maior que o limite superior!!');
+        return;
+    } else {
+
+        let numero;
+        let sorteados = [];
+
+        for (let i=1; i<=quantidade;i++){
             numero = gerarNumeroAleatorio(de,ate);
+            while(sorteados.includes(numero)){
+                numero = gerarNumeroAleatorio(de,ate);
+            }
+            sorteados.push(numero);  
         }
-        sorteados.push(numero);  
-    }
-        
 
-    let resultado = document.getElementById('resultado');
-    if (quantidade > 1) {
-        resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
-    }
-    else{
+        let resultado = document.getElementById('resultado');
+        if (quantidade > 1) {
+            resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+        }else{
             resultado.innerHTML = `<label class="texto__paragrafo">Número sorteado: ${sorteados}</label>`;
-    }
+        }
    
-    alterarStatusBotao();
+        alterarStatusBotao();
+    }
 }
 
 function reiniciar(){
