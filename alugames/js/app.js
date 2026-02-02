03c7c401a33769fba43ce4dc01907b93cdf81f29
Junior <jorge-luiz.junior@siemens.com>
2026-02-02 13:34:42 -0300

    Adição de nova resolução do desafio e código desenvolvido pela ALURA

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -43,3 +43,25 @@ function alterarStatus(id){
         botao.textContent = 'Alugar';
     }
 }
+
+/* Solução desenvolvida pela ALURA: A diferença é que no meu eu chego pelo texto Alugar ou Devolver referente ao botão,
+já na da Alura, eles checam a classe da imagem para saber se contém a classe referente ao jogo alugado 
+
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+}
+
+*/
