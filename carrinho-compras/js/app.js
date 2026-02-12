fbadd7a44e3c33d8aa0e090085c43fd81da78957
Jorge <workjorge19@gmail.com>
2026-02-12 12:14:41 -0300

    Projeto do carrinho de compras

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,32 @@
+let precoFinal;
+limpar();
+
+
+function adicionar(){
+   //Recupera produto, nome, preço da unidade, quantidade e calcula valor total 
+   let produto = document.getElementById('produto').value;
+   let nomeDoProduto = produto.split('-')[0];
+   let precoUnidade = produto.split('R$')[1];
+   let quantidade = document.getElementById('quantidade').value;
+   let preco = quantidade * precoUnidade;
+
+   //Adiciona produto no carrinho
+   let carrinho = document.getElementById('lista-produtos');
+   carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeDoProduto} <span class="texto-azul">R$${precoUnidade}</span>
+        </section>`;
+
+   //Calcula valor total do carrinho
+   precoFinal = precoFinal + preco;
+   let total = document.getElementById('valor-total');
+   total.textContent = `R$ ${precoFinal}`;
+   document.getElementById('quantidade').value = 0;
+}
+
+function limpar(){
+   precoFinal = 0;
+   document.getElementById('lista-produtos').innerHTML = '';
+   document.getElementById('valor-total').textContent = 'R$ 0';
+   document.getElementById('quantidade').value = 0;
+}
+
